8b5a3d8e6d47a58302a307316ae37fc5d70a19f3
1001hyena <145873382+1001hyena@users.noreply.github.com>
2024-05-08 22:24:53 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,23 +1,39 @@
+
+
+
 import discord
 from discord.ext import commands
 from bot_token import token
+
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+
+bot = commands.Bot(command_prefix='@hyenabot ', intents=intents)
+
+
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} olarak giriş yaptık')
 
+
+
 @bot.command()
-async def hello(ctx):
-    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+async def merhaba(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir botum ve bu sunucunun modiratörluğunu yapıyorum')
+
+
 
 @bot.command()
 async def heh(ctx, count_heh = 5):
-    await ctx.send("he" * count_heh)
+    await ctx.send("hahaha" * count_heh)
+
+
 
 bot.run(token)
+
+
